b445ae43becffaac9cca828e06880988654ae022
dkikowe <dkikowe@gmail.com>
2025-06-01 02:54:20 +0500

    sds

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const { runTelemetrParser } = require("./scheduler");
+// const { runTelemetrParser } = require("./scheduler");
 
 const {
   getChannelsByCategory,
